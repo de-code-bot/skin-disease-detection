2c70e34c27958004a4921c81ee2428b5005350fb
Parth Acharya <1008parth@gmail.com>
2025-08-30 14:05:16 +0530

    Updates file acquiring logic for POST /api/v1/predictions

--- backend/model_blueprint.py
+++ backend/model_blueprint.py
@@ -6,7 +6,7 @@ from backend.auxilary.decorators import enforce_mimetype
 
 from quart import Blueprint, Response, request, current_app, jsonify
 
-from werkzeug.datastructures import MultiDict
+from werkzeug.datastructures import MultiDict, FileStorage
 
 __all__ = ('MODEL_BLUEPRINT',
            'submit_prediction')
@@ -16,16 +16,19 @@ MODEL_BLUEPRINT: Final[Blueprint] = Blueprint("models_blueprint", "models_bluepr
 MODEL_BLUEPRINT.route('/', methods=['POST'])
 @enforce_mimetype(current_app.config['ACCEPTED_IMAGE_MIMETYPE'])
 async def submit_prediction() -> tuple[Response, int]:
-    files: MultiDict = await request.files
+    files: MultiDict[str, FileStorage] = await request.files
     additional_kwargs: dict[str, str] = {}
+    
     if len(files) > 1:
         additional_kwargs['file_quantity'] = "Only a single image accepted"
 
-    image = files[0]
+    input_file: Final[FileStorage] = next(iter(files.values()))
+    image_bytes: Final[bytes] = await input_file.read()
     
     # Some work that the model does idk
 
     response_data: dict[str, Any] = {'k' : 'v'}
-    response_data['additional'] = additional_kwargs
+    if additional_kwargs:
+        response_data['additional'] = additional_kwargs
 
     return (jsonify(response_data), 200)
