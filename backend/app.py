fe592a228a7b05f7efcaff6cdb1e25af92239456
Parth Acharya <1008parth@gmail.com>
2025-08-30 13:55:10 +0530

    Adds Quart app factory

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from quart import Quart
+from typing import Final
+
+from backend.config.app_config import make_config
+from backend.auxilary.utils import generic_error_handler
+
+def create_app() -> Quart:
+    '''Create an instance of the Quart app'''
+    
+    app: Final[Quart] = Quart(__name__)
+    app.config.from_object(make_config())
+
+    from model_blueprint import MODEL_BLUEPRINT
+    app.register_blueprint(MODEL_BLUEPRINT, url_prefix='/'.join([app.config['APPLICATION_ROOT'], 'predictions']))
+    
+    app.register_error_handler(Exception, generic_error_handler)
+
+    return app
