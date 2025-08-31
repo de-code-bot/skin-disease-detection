17ad91739b3e52f654611006df0f3b3c5a68560a
Parth Acharya <1008parth@gmail.com>
2025-08-31 13:30:23 +0530

    Updates decorator enforce_mimetype

--- backend/auxilary/decorators.py
+++ backend/auxilary/decorators.py
@@ -3,12 +3,13 @@ from quart import request, g
 from werkzeug.exceptions import BadRequest
 from typing import Callable
 
-def enforce_mimetype(mimetype: str):
+def enforce_mimetype(type_: str, subtype: str):
     def outer_decorator(endpoint: Callable):
         @wraps(endpoint)
         def inner_decorator(*args, **kwargs):
-            if request.mimetype != mimetype:
-                raise BadRequest(f"Request mimetype must be {mimetype}, got {request.mimetype} instead")
+            request_type, request_subtype = request.mimetype.split('/')
+            if request_type != type_ or (request_subtype != '*' and request_subtype != subtype):
+                raise BadRequest(f"Request media type must have type {type_}, with subtype {subtype}, got {request.mimetype} instead")
             return endpoint(*args, **kwargs)
         return inner_decorator
     return outer_decorator
