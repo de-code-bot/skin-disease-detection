f2408e2d7d4d2a28a6f0181ce74f44276a9ea045
Parth Acharya <1008parth@gmail.com>
2025-09-02 20:09:02 +0530

    Updates app factory to initialize global singletons before registering blueprints

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,26 @@
 from quart import Quart
+from pathlib import Path
 from typing import Final
 
-from backend.config.app_config import make_config
 from backend.auxilary.utils import generic_error_handler
+from backend.config.app_config import ServerConfig
+
+from backend import bootup
 
 def create_app() -> Quart:
     '''Create an instance of the Quart app'''
+    SERVER_ROOT_DIRECTORY: Final[Path] = Path(__file__).parent
     
-    app: Final[Quart] = Quart(__name__)
-    app.config.from_object(make_config())
+    app: Final[Quart] = Quart(__name__,
+                              root_path=str(SERVER_ROOT_DIRECTORY),
+                              instance_path=str(SERVER_ROOT_DIRECTORY / 'instance'))
+    app.config.from_object(bootup.init_app_config(SERVER_ROOT_DIRECTORY / 'config' / 'app_config.toml'))
 
+    # Initialize global singletons
+    server_config: Final[ServerConfig] = bootup.init_server_config(SERVER_ROOT_DIRECTORY / 'config' / 'server_config.toml')
+    
+    bootup.init_image_classifier(Path(app.instance_path) / Path(server_config.classifier_h5_filename))
+    
     from backend.blueprints.model_blueprint import MODEL_BLUEPRINT
     from backend.blueprints.template_blueprint import TEMPLATES_BLUEPRINT
     app.register_blueprint(MODEL_BLUEPRINT, url_prefix='/'.join([app.config['APPLICATION_ROOT'], 'predictions']))
