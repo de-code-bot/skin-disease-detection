a07acd0561c82a142dda4af81129891813732724
Parth Acharya <1008parth@gmail.com>
2025-09-04 22:53:29 +0530

    Updates db scripts to act as CLI tools, and use default filepath assumptions

--- backend/database/genesis.py
+++ backend/database/genesis.py
@@ -1,18 +1,27 @@
 '''Script for creating disease_classifications table'''
-import os
+import argparse
 import sqlite3
-import sys
-from typing import Final
+import tomllib
+from pathlib import Path
+from typing import Any, Optional
 
 __all__ = ('main',)
 
-def main(database_path: str) -> None:
+def main(database_path: Optional[str] = None) -> None:
+    if not database_path:
+        base_dir: Path = Path(__file__).parent.parent  # Traverse upwards to ./../backend, based on the assumption that an instance directory is at the same level as this package
+        config_dict: dict[str, dict[str, Any]] = tomllib.loads((base_dir / 'config' / 'server_config.toml').read_text(encoding='utf-8'))
+        database_path = str(base_dir / 'instance' / config_dict.get('database', {}).get('database_path', 'test.db'))
+    
     with sqlite3.connect(database_path) as conn:
-        cursor: Final[sqlite3.Cursor] = conn.cursor()
-        with open(os.path.join(os.path.dirname(__file__), 'genesis.sql'), 'r') as genesis_script:
-            cursor.executescript(genesis_script.read())
-        
+        cursor: sqlite3.Cursor = conn.cursor()
+        sql_script: str = (Path(__file__).parent / 'genesis.sql').read_text(encoding='utf-8')
+        cursor.executescript(sql_script)
         conn.commit()
 
+argument_parser: argparse.ArgumentParser = argparse.ArgumentParser('db_genesis')
+argument_parser.add_argument('--database-path')
+
 if __name__ == '__main__':
-    main(sys.argv[1])
+    parsed_args: argparse.Namespace = argument_parser.parse_args()
+    main(**parsed_args.__dict__)
