a3f07c60dc39c023c78d9ad40c68c9fb97c7e9cb
Parth Acharya <1008parth@gmail.com>
2025-09-04 18:21:56 +0530

    Updates schema for image_logs

--- backend/database/genesis.sql
+++ backend/database/genesis.sql
@@ -6,8 +6,10 @@ CREATE TABLE disease_classifications(
 CREATE TABLE image_logs(
     id INTEGER PRIMARY KEY,
     image_name VARCHAR(128) NOT NULL,
-    extension VARCHAR(4) NOT NULL,   
+    extension VARCHAR(4) NOT NULL,
+    image_path VARCHAR(256) NOT NULL UNIQUE,
     classification VARCHAR(32) NOT NULL,
+    processed_at TIMESTAMP NOT NULL DEFAULT datetime,
     FOREIGN KEY (classification) REFERENCES disease_classifications(disease_name)
 );
 
