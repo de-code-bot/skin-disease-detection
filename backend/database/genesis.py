b142d71240a53a0fce5e70fe9916fe5cb21d67ad
Ahaan Mehta <mehtaahaan@gmail.com>
2025-09-16 09:28:24 +0530

    Update genesis.py

--- backend/database/genesis.py
+++ backend/database/genesis.py
@@ -13,6 +13,9 @@ def main(database_path: Optional[str] = None) -> None:
         config_dict: dict[str, dict[str, Any]] = tomllib.loads((base_dir / 'config' / 'server_config.toml').read_text(encoding='utf-8'))
         database_path = str(base_dir / 'instance' / config_dict.get('database', {}).get('database_path', 'test.db'))
     
+    if not Path(database_path).exists():
+    Path(database_path).parent.mkdir(parents=True)
+    
     with sqlite3.connect(database_path) as conn:
         cursor: sqlite3.Cursor = conn.cursor()
         sql_script: str = (Path(__file__).parent / 'genesis.sql').read_text(encoding='utf-8')
